Fix bug in group size calculation during encoding, add pruning and range quantization

adaptiveleak/attack.py
@@ -6,9 +6,10 @@ from collections import defaultdict, namedtuple
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
 from sklearn.ensemble import AdaBoostClassifier
+from sklearn.svm import SVC
 from typing import Any, Dict, Tuple, List
 
-from utils.file_utils import read_pickle_gz, save_pickle_gz
+from utils.file_utils import read_json_gz, save_json_gz
 
 
 AttackResult = namedtuple('AttackResult', ['train_accuracy', 'num_train', 'test_accuracy', 'num_test', 'most_freq_accuracy'])
@@ -16,7 +17,10 @@ AttackResult = namedtuple('AttackResult', ['train_accuracy', 'num_train', 'test_
 
 def create_dataset(policy_result: Dict[str, Any], window_size: int, stride: int) -> Tuple[np.ndarray, np.ndarray]:
 
-    bytes_dist = policy_result['byte_dist']
+    bytes_dist: DefaultDict[int, List[int]] = defaultdict(list)
+
+    for label, num_bytes in zip(policy_result['labels'], policy_result['num_bytes']):
+        bytes_dist[label].append(num_bytes)
 
     inputs: List[np.ndarray] = []
     output: List[int] = []
@@ -53,7 +57,8 @@ def fit_attack_model(inputs: np.ndarray, output: np.ndarray, train_frac: float):
     train_inputs, test_inputs = model_inputs[train_idx], model_inputs[test_idx]
     train_output, test_output = output[train_idx], output[test_idx]
 
-    clf = MLPClassifier(hidden_layer_sizes=[32, 32], alpha=0.1, max_iter=10000, random_state=rand)
+    clf = MLPClassifier(hidden_layer_sizes=[32], alpha=0.1, max_iter=10000, random_state=rand)
+    # clf = SVC(C=2.0, kernel='rbf')
     clf.fit(train_inputs, train_output)
 
     train_accuracy = clf.score(train_inputs, train_output)
@@ -85,7 +90,7 @@ if __name__ == '__main__':
     for policy_file in args.policy_files:
 
         if os.path.isdir(policy_file):
-            file_names = [name for name in os.listdir(policy_file) if name.endswith('.pkl.gz')]
+            file_names = [name for name in os.listdir(policy_file) if name.endswith('.json.gz')]
             policy_files.extend((os.path.join(policy_file, name) for name in file_names))
         else:
             policy_files.append(policy_file)
@@ -95,7 +100,7 @@ if __name__ == '__main__':
         print('Starting {0}'.format(policy_file))
         print('==========')
         
-        policy_result = read_pickle_gz(policy_file)
+        policy_result = read_json_gz(policy_file)
 
         inputs, output = create_dataset(policy_result, window_size=args.window_size, stride=args.stride)
 
@@ -103,4 +108,4 @@ if __name__ == '__main__':
 
         # Save the attack result
         policy_result['attack'] = attack_result._asdict()
-        save_pickle_gz(policy_result, policy_file)
+        save_json_gz(policy_result, policy_file)

adaptiveleak/policies.py
@@ -4,8 +4,9 @@ from enum import Enum, auto
 from typing import Tuple, List, Dict, Any
 
 from adaptiveleak.controllers import PIDController
-from adaptiveleak.utils.constants import BIT_WIDTH
+from adaptiveleak.utils.constants import BIT_WIDTH, MIN_WIDTH
 from adaptiveleak.utils.data_utils import get_group_widths, get_num_groups, calculate_bytes, pad_to_length
+from adaptiveleak.utils.data_utils import prune_sequence, get_max_collected, calculate_grouped_bytes
 from adaptiveleak.utils.message import encode_byte_measurements, decode_byte_measurements
 from adaptiveleak.utils.message import encode_grouped_measurements, decode_grouped_measurements
 from adaptiveleak.utils.encryption import AES_BLOCK_SIZE, EncryptionMode, CHACHA_NONCE_LEN
@@ -109,6 +110,7 @@ class AdaptivePolicy(Policy):
                  width: int,
                  seq_length: int,
                  num_features: int,
+                 window: int,
                  encryption_mode: EncryptionMode,
                  encoding_mode: EncodingMode):
         super().__init__(precision=precision,
@@ -130,7 +132,10 @@ class AdaptivePolicy(Policy):
         self._pid = PIDController(kp=(1.0 / 16.0),
                                   ki=(1.0 / 32.0),
                                   kd=(1.0 / 128.0))
+
+        self._start_threshold = threshold
         self._threshold = threshold
+        self._window = window
 
     @property
     def encoding_mode(self) -> EncodingMode:
@@ -170,8 +175,8 @@ class AdaptivePolicy(Policy):
         avg = self._measurement_count / (self._seq_count * self._seq_length)
         signal = self._pid.step(estimate=avg, target=self._target)
 
-        if (seq_idx % 20) == 0:
-            self._threshold = -1 * signal
+        if ((seq_idx + 1) % self._window) == 0:
+            self._threshold = self._start_threshold - signal
 
     def encode(self, measurements: np.ndarray, collected_indices: List[int]) -> bytes:
         if self.encoding_mode == EncodingMode.STANDARD:
@@ -190,22 +195,42 @@ class AdaptivePolicy(Policy):
                                       target_frac=self.target,
                                       encryption_mode=self.encryption_mode)
 
+            # Get the maximum possible number of collected measurements
+            target_bytes = calculate_bytes(width=BIT_WIDTH,
+                                           num_collected=int(self.target * self.seq_length),
+                                           num_features=self.num_features,
+                                           seq_length=self.seq_length,
+                                           encryption_mode=self.encryption_mode)
+
+            max_collected = get_max_collected(seq_length=self.seq_length,
+                                              num_features=self.num_features,
+                                              group_size=self.group_size,
+                                              min_width=MIN_WIDTH,
+                                              target_size=target_bytes,
+                                              encryption_mode=self.encryption_mode)
+
+            # Prune away any excess measurements
+            measurements, collected_indices = prune_sequence(measurements=measurements,
+                                                             collected_indices=collected_indices,
+                                                             max_collected=max_collected)
+
+            # Encode the measurement values
             non_fractional = self.width - self.precision
             encoded = encode_grouped_measurements(measurements=measurements,
                                                   collected_indices=collected_indices,
                                                   seq_length=self.seq_length,
                                                   widths=widths,
-                                                  non_fractional=non_fractional)
+                                                  non_fractional=non_fractional,
+                                                  group_size=self.group_size)
+
+            expected_bytes = calculate_grouped_bytes(widths=widths,
+                                                     num_collected=num_collected,
+                                                     num_features=self.num_features,
+                                                     group_size=self.group_size,
+                                                     seq_length=self.seq_length,
+                                                     encryption_mode=self.encryption_mode)
 
             if self.encryption_mode == EncryptionMode.STREAM:
-                target_bytes = calculate_bytes(width=BIT_WIDTH,
-                                               num_collected=int(self.target * self.seq_length),
-                                               num_features=self.num_features,
-                                               seq_length=self.seq_length,
-                                               encryption_mode=self.encryption_mode)
-
-                print('{0} {1} {2}'.format(widths, target_bytes, len(encoded)))
-
                 return pad_to_length(encoded, length=target_bytes - CHACHA_NONCE_LEN)
 
             return encoded
@@ -221,7 +246,8 @@ class AdaptivePolicy(Policy):
             return decode_grouped_measurements(encoded=message,
                                                seq_length=self.seq_length,
                                                num_features=self.num_features,
-                                               non_fractional=non_fractional)
+                                               non_fractional=non_fractional,
+                                               group_size=self.group_size)
         else:
             raise ValueError('Unknown encoding type {0}'.format(self.encoding_mode.name))
 
@@ -349,7 +375,8 @@ def make_policy(name: str, seq_length: int, num_features: int, precision: int, e
                               seq_length=seq_length,
                               num_features=num_features,
                               encryption_mode=encryption_mode,
-                              encoding_mode=EncodingMode[str(kwargs['encoding']).upper()])
+                              encoding_mode=EncodingMode[str(kwargs['encoding']).upper()],
+                              window=50)
     elif name == 'uniform':
         return UniformPolicy(target=target,
                              precision=precision,

adaptiveleak/server.py
@@ -10,7 +10,7 @@ from adaptiveleak.policies import make_policy, Policy
 from adaptiveleak.utils.encryption import decrypt, EncryptionMode
 from adaptiveleak.utils.message import decode_byte_measurements
 from adaptiveleak.utils.loading import load_data
-from adaptiveleak.utils.file_utils import read_json, save_json_gz
+from adaptiveleak.utils.file_utils import read_json, save_json_gz, read_pickle_gz
 
 
 def reconstruct_sequence(measurements: np.ndarray, collected_indices: List[int], seq_length: int) -> np.ndarray:
@@ -131,10 +131,6 @@ class Server:
                     error = mean_squared_error(y_true=inputs[idx],
                                                y_pred=reconstructed)
 
-                    if (idx == 495):
-                        print(reconstructed)
-                        print(error)
-
                     # Log the results of this sequence
                     num_bytes.append(len(recv_sample))
                     num_measurements.append(len(measurements))
@@ -182,7 +178,11 @@ if __name__ == '__main__':
 
     # Extract the parameters
     params = read_json(args.params)
-    
+
+    # Get preset threshold (if present)
+    threshold_path = os.path.join('saved_models', args.dataset, 'thresholds.pkl.gz')
+    thresholds = read_pickle_gz(threshold_path) if os.path.exists(threshold_path) else dict()
+
     # Make the policy
     policy = make_policy(name=params['name'],
                          target=params['target'],
@@ -190,7 +190,8 @@ if __name__ == '__main__':
                          num_features=inputs.shape[2],
                          seq_length=inputs.shape[1],
                          encryption_mode=encryption_mode,
-                         encoding=params.get('encoding', 'unknown'))
+                         encoding=params.get('encoding', 'unknown'),
+                         threshold=thresholds.get(params['target'], 0.0))
 
     # Run the experiment
     server.run(inputs=inputs,

adaptiveleak/tests/utils/data_utils.py
@@ -527,7 +527,8 @@ class TestCalculateBytes(unittest.TestCase):
                                               collected_indices=collected_indices,
                                               widths=widths,
                                               seq_length=seq_length,
-                                              non_fractional=non_fractional)
+                                              non_fractional=non_fractional,
+                                              group_size=group_size)                                              
 
         key = get_random_bytes(32)
         encrypted = encrypt(message=encoded, key=key, mode=EncryptionMode.STREAM)
@@ -554,7 +555,8 @@ class TestCalculateBytes(unittest.TestCase):
                                               collected_indices=collected_indices,
                                               widths=widths,
                                               seq_length=seq_length,
-                                              non_fractional=non_fractional)
+                                              non_fractional=non_fractional,
+                                              group_size=group_size)
 
         key = get_random_bytes(32)
         encrypted = encrypt(message=encoded, key=key, mode=EncryptionMode.STREAM)
@@ -581,7 +583,8 @@ class TestCalculateBytes(unittest.TestCase):
                                               collected_indices=collected_indices,
                                               widths=widths,
                                               seq_length=seq_length,
-                                              non_fractional=non_fractional)
+                                              non_fractional=non_fractional,
+                                              group_size=group_size)
 
         key = get_random_bytes(AES_BLOCK_SIZE)
         encrypted = encrypt(message=encoded, key=key, mode=EncryptionMode.BLOCK)
@@ -608,7 +611,8 @@ class TestCalculateBytes(unittest.TestCase):
                                               collected_indices=collected_indices,
                                               widths=widths,
                                               seq_length=seq_length,
-                                              non_fractional=non_fractional)
+                                              non_fractional=non_fractional,
+                                              group_size=group_size)
 
         key = get_random_bytes(AES_BLOCK_SIZE)
         encrypted = encrypt(message=encoded, key=key, mode=EncryptionMode.BLOCK)
@@ -747,6 +751,25 @@ class TestMaxGroups(unittest.TestCase):
                                                           seq_length=seq_length)
 
         self.assertLessEqual(grouped_size, target_size)
+
+
+class TestPruning(unittest.TestCase):
+
+
+    def test_prune_two(self):
+        measurements = np.array([[1.0, 1.0], [1.0, 1.0], [2.0, 2.0], [2.5, 4.0], [3.5, 3.0]])
+        max_collected = 3
+        collected_indices = [1, 3, 5, 9, 10]
+
+        pruned_features, pruned_indices = data_utils.prune_sequence(measurements=measurements,
+                                                                    max_collected=max_collected,
+                                                                    collected_indices=collected_indices)
+
+        expected_features = np.array([[1.0, 1.0], [2.5, 4.0], [3.5, 3.0]])
+        expected_indices = [1, 9, 10]
+
+        self.assertTrue(np.all(np.isclose(pruned_features, expected_features)))
+        self.assertEqual(pruned_indices, expected_indices)
 
 
 if __name__ == '__main__':

adaptiveleak/tests/utils/message.py
@@ -66,17 +66,20 @@ class TestGroups(unittest.TestCase):
         seq_length = 8
         collected_indices = [0, 1]
         widths = [6, 5]
+        group_size = 3
 
         encoded = message.encode_grouped_measurements(measurements=measurements,
                                                       collected_indices=collected_indices,
                                                       seq_length=seq_length,
                                                       widths=widths,
-                                                      non_fractional=non_fractional)
+                                                      non_fractional=non_fractional,
+                                                      group_size=group_size)
 
         decoded, indices = message.decode_grouped_measurements(encoded=encoded,
                                                                seq_length=seq_length,
                                                                num_features=measurements.shape[1],
-                                                               non_fractional=non_fractional)
+                                                               non_fractional=non_fractional,
+                                                               group_size=group_size)
 
         # Check recovered values
         self.assertTrue(np.all(np.isclose(decoded, measurements)))
@@ -92,17 +95,20 @@ class TestGroups(unittest.TestCase):
         seq_length = 8
         collected_indices = [0, 5]
         widths = [6, 4]
+        group_size = 3
 
         encoded = message.encode_grouped_measurements(measurements=measurements,
                                                       collected_indices=collected_indices,
                                                       seq_length=seq_length,
                                                       widths=widths,
-                                                      non_fractional=non_fractional)
+                                                      non_fractional=non_fractional,
+                                                      group_size=group_size)
 
         decoded, indices = message.decode_grouped_measurements(encoded=encoded,
                                                                seq_length=seq_length,
                                                                num_features=measurements.shape[1],
-                                                               non_fractional=non_fractional)
+                                                               non_fractional=non_fractional,
+                                                               group_size=group_size)
 
         # Check recovered values
         self.assertTrue(np.all(np.isclose(decoded, measurements)))
@@ -118,17 +124,20 @@ class TestGroups(unittest.TestCase):
         seq_length = 8
         collected_indices = [0, 7]
         widths = [6, 5, 5]
+        group_size = 2
 
         encoded = message.encode_grouped_measurements(measurements=measurements,
                                                       collected_indices=collected_indices,
                                                       seq_length=seq_length,
                                                       widths=widths,
-                                                      non_fractional=non_fractional)
+                                                      non_fractional=non_fractional,
+                                                      group_size=2)
 
         decoded, indices = message.decode_grouped_measurements(encoded=encoded,
                                                                seq_length=seq_length,
                                                                num_features=measurements.shape[1],
-                                                               non_fractional=non_fractional)
+                                                               non_fractional=non_fractional,
+                                                               group_size=2)
 
         # Check recovered values
         self.assertTrue(np.all(np.isclose(decoded, measurements)))
@@ -144,19 +153,22 @@ class TestGroups(unittest.TestCase):
         seq_length = 8
         collected_indices = [0, 7]
         widths = [6, 5, 5]
+        group_size = 2
 
         encoded = message.encode_grouped_measurements(measurements=measurements,
                                                       collected_indices=collected_indices,
                                                       seq_length=seq_length,
                                                       widths=widths,
-                                                      non_fractional=non_fractional)
+                                                      non_fractional=non_fractional,
+                                                      group_size=group_size)
 
         padded = pad_to_length(encoded, length=len(encoded) + 6)
 
         decoded, indices = message.decode_grouped_measurements(encoded=padded,
                                                                seq_length=seq_length,
                                                                num_features=measurements.shape[1],
-                                                               non_fractional=non_fractional)
+                                                               non_fractional=non_fractional,
+                                                               group_size=group_size)
 
         # Check recovered values
         self.assertTrue(np.all(np.isclose(decoded, measurements)))
@@ -175,17 +187,20 @@ class TestGroups(unittest.TestCase):
         seq_length = inputs.shape[0]
         collected_indices = list(range(seq_length))
         non_fractional = 2
+        group_size = 100
 
         encoded = message.encode_grouped_measurements(measurements=inputs,
                                                       collected_indices=collected_indices,
                                                       seq_length=seq_length,
                                                       widths=widths,
-                                                      non_fractional=non_fractional)
+                                                      non_fractional=non_fractional,
+                                                      group_size=group_size)
 
         decoded, indices = message.decode_grouped_measurements(encoded=encoded,
                                                                seq_length=seq_length,
                                                                num_features=inputs.shape[1],
-                                                               non_fractional=non_fractional)
+                                                               non_fractional=non_fractional,
+                                                               group_size=group_size)
 
         error = np.average(np.sum(np.square(decoded - inputs), axis=-1))
         self.assertLessEqual(error, 0.01)
@@ -199,17 +214,20 @@ class TestGroups(unittest.TestCase):
         seq_length = inputs.shape[0]
         collected_indices = list(range(seq_length))
         non_fractional = 2
+        group_size = 100
 
         encoded = message.encode_grouped_measurements(measurements=inputs,
                                                       collected_indices=collected_indices,
                                                       seq_length=seq_length,
                                                       widths=widths,
-                                                      non_fractional=non_fractional)
+                                                      non_fractional=non_fractional,
+                                                      group_size=group_size)
 
         decoded, indices = message.decode_grouped_measurements(encoded=encoded,
                                                                seq_length=seq_length,
                                                                num_features=inputs.shape[1],
-                                                               non_fractional=non_fractional)
+                                                               non_fractional=non_fractional,
+                                                               group_size=group_size)
 
         error = np.average(np.sum(np.square(decoded - inputs), axis=-1))
         self.assertLessEqual(error, 0.01)
@@ -223,19 +241,22 @@ class TestGroups(unittest.TestCase):
         seq_length = inputs.shape[0]
         collected_indices = list(range(seq_length))
         non_fractional = 2
+        group_size = 100
 
         encoded = message.encode_grouped_measurements(measurements=inputs,
                                                       collected_indices=collected_indices,
                                                       seq_length=seq_length,
                                                       widths=widths,
-                                                      non_fractional=non_fractional)
+                                                      non_fractional=non_fractional,
+                                                      group_size=group_size)
 
         padded = pad_to_length(encoded, len(encoded) + 6)
 
         decoded, indices = message.decode_grouped_measurements(encoded=padded,
                                                                seq_length=seq_length,
                                                                num_features=inputs.shape[1],
-                                                               non_fractional=non_fractional)
+                                                               non_fractional=non_fractional,
+                                                               group_size=group_size)
 
         error = np.average(np.sum(np.square(decoded - inputs), axis=-1))
         self.assertLessEqual(error, 0.01)

adaptiveleak/utils/data_utils.py
@@ -4,7 +4,7 @@ from functools import partial
 from Cryptodome.Random import get_random_bytes
 from typing import List, Union, Tuple
 
-from adaptiveleak.utils.constants import BIT_WIDTH, BIG_NUMBER
+from adaptiveleak.utils.constants import BIT_WIDTH, BIG_NUMBER, MIN_WIDTH
 from adaptiveleak.utils.encryption import AES_BLOCK_SIZE, EncryptionMode, CHACHA_NONCE_LEN
 
 
@@ -330,6 +330,7 @@ def get_group_widths(group_size: int,
     while (i < MAX_ITER):
         # Adjust the group width
         widths[group_idx] += 1 if data_bytes <= target_bytes else -1
+        widths[group_idx] = max(widths[group_idx], MIN_WIDTH)
 
         # Update the byte count
         updated_bytes = calculate_grouped_bytes(widths=widths,
@@ -341,7 +342,7 @@ def get_group_widths(group_size: int,
 
         # Exit the loop when we find the inflection point
         if (data_bytes <= target_bytes and updated_bytes > target_bytes):
-            widths[group_idx] -= 1
+            widths[group_idx] = max(widths[group_idx] - 1, MIN_WIDTH)
             break
 
         data_bytes = updated_bytes
@@ -453,25 +454,31 @@ def prune_sequence(measurements: np.ndarray, collected_indices: List[int], max_c
     assert len(measurements.shape) == 2, 'Must provide a 2d array of measurements'
     assert measurements.shape[0] == len(collected_indices), 'Misaligned measurements ({0}) and collected indices ({1})'.format(measurements.shape[0], len(collected_indices))
 
+    # Avoid pruning measurements which are under budget
     num_collected = len(collected_indices)
     if num_collected <= max_collected:
         return measurements, collected_indices
 
+    # Calculate the local measurement error (assuming removal)
     ahead_errors = np.sum(np.abs(measurements[1:] - measurements[:-1]), axis=-1)  # [K - 1]
     total_errors = ahead_errors[1:] + ahead_errors[:-1]  # [K - 2]
 
+    # Partition the indices based on error
     num_to_remove = num_collected - max_collected
     partitioned = np.argpartition(total_errors, kth=num_to_remove) + 1
 
+    # Keep the measurements with the highest induced error
     highest_error_indices = partitioned[num_to_remove:]
     kept_measurements = measurements[highest_error_indices]
 
+    # Always include the first and last measurements
     first = np.expand_dims(measurements[0], axis=0)
     last = np.expand_dims(measurements[-1], axis=0)
 
+    # Collect the pruned measurements and indices
     pruned = np.vstack([first, kept_measurements, last])
 
     first_idx, last_idx = collected_indices[0], collected_indices[-1]
-    kept_indices = [first_idx] + [i for i in highest_error_indices] + [last_idx]
+    kept_indices = [first_idx] + [collected_indices[i] for i in highest_error_indices] + [last_idx]
 
     return pruned, kept_indices

adaptiveleak/utils/loading.py
@@ -4,7 +4,7 @@ import numpy as np
 from typing import Tuple
 
 
-def load_data(dataset_name: str, fold: str, ) -> Tuple[np.ndarray, np.ndarray]:
+def load_data(dataset_name: str, fold: str) -> Tuple[np.ndarray, np.ndarray]:
     """
     Loads the dataset inputs and labels.
 

adaptiveleak/utils/message.py
@@ -121,7 +121,7 @@ def decode_byte_measurements(byte_str: bytes, seq_length: int, num_features: int
     return decoded.reshape(-1, num_features), collected_indices
 
 
-def encode_grouped_measurements(measurements: np.ndarray, collected_indices: List[int], widths: List[int], seq_length: int, non_fractional: int) -> bytes:
+def encode_grouped_measurements(measurements: np.ndarray, collected_indices: List[int], widths: List[int], seq_length: int, non_fractional: int, group_size: int) -> bytes:
     """
     Encodes the measurements into sets of grouped features with different widths.
 
@@ -132,6 +132,7 @@ def encode_grouped_measurements(measurements: np.ndarray, collected_indices: Lis
         seq_length: The length of the full sequence (T)
         widths: A list of bit-widths for features in each group
         non_fractional: The number of non-fractional bits per value
+        group_size: The number of measurements per group
     Returns:
         A hex string that represents the encoded measurements.
     """
@@ -142,12 +143,10 @@ def encode_grouped_measurements(measurements: np.ndarray, collected_indices: Lis
     num_measurements = len(measurements)
     shifts: List[int] = []
 
-    # group_metadata = bytes([num_measurements, num_groups]) + bytes(widths)
-
     # Divide features into groups and encode separately
     flattened = measurements.reshape(-1)  # [K * D]
 
-    group_size = int(math.ceil((measurements.shape[0] * measurements.shape[1]) / num_groups))
+    # group_size = int(math.ceil((measurements.shape[0] * measurements.shape[1]) / num_groups))
 
     encoded_groups: List[bytes] = []
     for group_idx, width in enumerate(widths):
@@ -195,7 +194,7 @@ def encode_grouped_measurements(measurements: np.ndarray, collected_indices: Lis
     return bytes(collected_mask) + group_metadata + encoded
 
 
-def decode_grouped_measurements(encoded: bytes, seq_length: int, num_features: int, non_fractional: int) -> Tuple[np.ndarray, List[int]]:
+def decode_grouped_measurements(encoded: bytes, seq_length: int, num_features: int, non_fractional: int, group_size: int) -> Tuple[np.ndarray, List[int]]:
     """
     Decodes the encoded group of measurements into the raw measurement values and collected
     sequence indices.
@@ -205,6 +204,7 @@ def decode_grouped_measurements(encoded: bytes, seq_length: int, num_features: i
         seq_length: The true sequence length
         num_features: The number of features per measurement
         non_fractional: The number of non-fractional bits
+        group_size: The number of measurements in each group (except for the last)
     Returns:
         A tuple of two values:
             (1) A [K, D] array of measurement values
@@ -231,7 +231,7 @@ def decode_grouped_measurements(encoded: bytes, seq_length: int, num_features: i
 
     start = 0
     count = 0
-    group_size = int(math.ceil((num_measurements * num_features) / num_groups))
+    # group_size = int(math.ceil((num_measurements * num_features) / num_groups))
 
     features: List[float] = []
 