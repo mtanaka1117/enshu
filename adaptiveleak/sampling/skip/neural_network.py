Fixed Skip RNN sampler to use an average rate

adaptiveleak/sampling/skip/neural_network.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 import numpy as np
 import sklearn.metrics as metrics
 import time
+import math
 from datetime import datetime
 from sklearn.preprocessing import StandardScaler
 from typing import Dict, Any, Optional, List, Tuple, Set, Union
@@ -12,10 +13,10 @@ from adaptiveleak.utils.file_utils import save_pickle_gz, read_pickle_gz, make_d
 
 
 DEFAULT_HYPERS = {
-    'batch_size': 16,
+    'batch_size': 32,
     'learning_rate': 0.001,
     'gradient_clip': 1,
-    'num_epochs': 10,
+    'num_epochs': 15,
     'patience': 10
 }
 
@@ -330,7 +331,8 @@ class NeuralNetwork:
 
         # Create the sample indices for batch creation
         train_idx = np.arange(len(train_inputs))
-        val_idx = np.arange(len(val_inputs))
+        num_train_batches = int(math.ceil(train_inputs.shape[0] / self.batch_size))
+        num_val_batches = int(math.ceil(val_inputs.shape[0] / self.batch_size))
 
         for epoch in range(self.num_epochs):
 
@@ -349,8 +351,8 @@ class NeuralNetwork:
             train_start = time.time()
 
             train_ops = [LOSS_OP, OPTIMIZER_OP]
-            for batch_idx in range(0, len(train_idx), self.batch_size):
-                start, end = batch_idx, batch_idx + self.batch_size
+            for batch_idx in range(num_train_batches):
+                start, end = batch_idx * self.batch_size, (batch_idx + 1) * self.batch_size
                 
                 indices = train_idx[start:end]
                 batch_features = train_inputs[indices]
@@ -373,7 +375,7 @@ class NeuralNetwork:
 
                 if should_print:
                     train_loss_so_far = epoch_train_loss / num_train_samples
-                    print('Train Batch {0}: Loss -> {1:.5f}'.format(batch_idx + 1, train_loss_so_far), end='\r')
+                    print('Train Batch {0} / {1}: Loss -> {2:.5f}'.format(batch_idx + 1, num_train_batches, train_loss_so_far), end='\r')
 
             # Clear the line after the epoch
             if should_print:
@@ -392,9 +394,8 @@ class NeuralNetwork:
             val_start = time.time()
 
             val_ops = [LOSS_OP]
-            for batch_idx in range(0, len(val_inputs), self.batch_size):
-
-                start, end = batch_idx, batch_idx + self.batch_size
+            for batch_idx in range(num_val_batches):
+                start, end = batch_idx * self.batch_size, (batch_idx + 1 ) * self.batch_size
                 batch_features = val_inputs[start:end]
 
                 feed_dict = self.batch_to_feed_dict(batch_features, is_train=False)
@@ -407,7 +408,7 @@ class NeuralNetwork:
 
                 if should_print:
                     val_loss_so_far = epoch_val_loss / num_val_samples
-                    print('Validation Batch {0}: Loss -> {1:.5f}'.format(batch_idx + 1, val_loss_so_far), end='\r')
+                    print('Validation Batch {0} / {1}: Loss -> {2:.5f}'.format(batch_idx + 1, num_val_batches, val_loss_so_far), end='\r')
 
             if should_print:
                 print()

adaptiveleak/sampling/skip/skip_rnn.py
@@ -6,11 +6,15 @@ Paper: https://arxiv.org/abs/1708.06834
 Repo: https://github.com/imatge-upc/skiprnn-2017-telecombcn/blob/master/src/rnn_cells/skip_rnn_cells.py.
 """
 import tensorflow as tf
-from collections import namedtuple
-from typing import Optional, Any, Tuple
+import numpy as np
+from collections import namedtuple, defaultdict
+from sklearn.metrics import mean_absolute_error
+from typing import Optional, Any, Dict, Tuple
 
 from adaptiveleak.utils.constants import SMALL_NUMBER
+from adaptiveleak.server import reconstruct_sequence
 from neural_network import NeuralNetwork, PREDICTION_OP, LOSS_OP, INPUTS
+from tfutils import apply_noise
 
 
 SkipUGRNNStateTuple = namedtuple('SkipUGRNNStateTuple', ['state', 'prev_input', 'cumulative_state_update'])
@@ -106,6 +110,9 @@ class SkipUGRNNCell(tf.compat.v1.nn.rnn_cell.RNNCell):
 
             next_cell_state = update_gate * candidate + (1.0 - update_gate) * prev_state
 
+            # Apply a small amount of noise for regularization
+            next_cell_state = apply_noise(next_cell_state, scale=0.01)
+
             # Apply the state update gate. This is the Skip portion.
             # We first compute the state update gate. This is a binary version of the cumulative state update prob.
             state_update_gate = binarize(prev_cum_state_update_prob)  # A [B, 1] binary tensor
@@ -164,14 +171,51 @@ class SkipRNN(NeuralNetwork):
         prediction = self._ops[PREDICTION_OP]
         expected = self._placeholders[INPUTS]
 
-        print('Prediction: {}'.format(prediction))
-        print('Expected: {}'.format(expected))
-
         squared_diff = tf.square(prediction - expected)
         pred_loss = tf.reduce_mean(squared_diff)
 
         num_updates = tf.reduce_sum(self._ops['skip_gates'], axis=-1)  # [B]
-        update_rate = num_updates / self.seq_length
-        update_loss = self._hypers['update_weight'] * tf.reduce_mean(tf.square(update_rate - self.target))
+        update_rate = num_updates / self.seq_length  # [B]
+        avg_update_rate = tf.reduce_mean(update_rate)
+
+        update_loss = self._hypers['update_weight'] * tf.square(tf.nn.leaky_relu(avg_update_rate - self.target))
 
         self._ops[LOSS_OP] = pred_loss + update_loss
+
+    def reconstruct(self, inputs: np.ndarray, labels: np.ndarray) -> Tuple[float, float, Dict[str, Tuple[float, float]]]:
+        # Compute the skip gates, [N, T]
+        feed_dict = self.batch_to_feed_dict(inputs, is_train=False)
+        model_result = self.execute('skip_gates', feed_dict=feed_dict)
+        skip_gates = model_result['skip_gates']
+
+        errors: List[float] = []
+        num_collected: List[float] = []
+
+        label_sizes: DefaultDict[int, List[int]] = defaultdict(list)
+
+        for gates, seq_inputs, label in zip(skip_gates, inputs, labels):
+            collected_indices = [idx for idx in range(self.seq_length) if np.isclose(gates[idx], 1)]
+            measurements = seq_inputs[collected_indices]
+
+            reconstructed = reconstruct_sequence(measurements=measurements,
+                                                 collected_indices=collected_indices,
+                                                 seq_length=self.seq_length)
+
+            error = mean_absolute_error(y_true=seq_inputs,
+                                        y_pred=reconstructed)
+
+            errors.append(error)
+            num_collected.append(len(collected_indices))
+
+            label_sizes[label].append(len(collected_indices))
+
+        avg_error = np.average(errors)
+        collection_rate = np.average(num_collected) / self.seq_length
+
+        label_stats: Dict[str, Tuple[float, float]] = dict()
+        for label in label_sizes.keys():
+            avg_size = np.average(label_sizes[label])
+            std_size = np.std(label_sizes[label])
+            label_stats[str(label)] = (float(avg_size), float(std_size))
+
+        return avg_error, collection_rate, label_stats