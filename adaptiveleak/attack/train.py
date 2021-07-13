049fd0115a22941b77ab59e6c545cec5ef068c3c
Tejas Kannan <tkannan@berkeley.edu>
2021-07-13 15:35:05 -0500

    Update attack model to use cross validation and decision trees

--- adaptiveleak/attack/train.py
+++ adaptiveleak/attack/train.py
@@ -2,18 +2,52 @@ import numpy as np
 import os
 from argparse import ArgumentParser
 from collections import defaultdict, namedtuple
-from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import AdaBoostClassifier
 from sklearn.model_selection import KFold
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from typing import Any, Dict, Tuple, List, DefaultDict
 
-from classifier import AttackClassifier
 from adaptiveleak.utils.analysis import geometric_mean
 from adaptiveleak.utils.file_utils import read_json_gz, save_json_gz, iterate_dir, make_dir
 
 
-NUM_EPOCHS = 1
-AttackResult = namedtuple('AttackResult', ['train_accuracy', 'num_train', 'test_accuracy', 'num_test', 'most_freq_accuracy'])
+AttackResult = namedtuple('AttackResult', ['accuracy', 'precision', 'recall', 'f1'])
+
+
+class AttackResultList:
+
+    def __init__(self):
+        self.accuracy: List[float] = []
+        self.precision: List[float] = []
+        self.recall: List[float] = []
+        self.f1: List[float] = []
+
+    def append(self, result: AttackResult):
+        self.accuracy.append(result.accuracy)
+        self.precision.append(result.precision)
+        self.recall.append(result.recall)
+        self.f1.append(result.f1)
+
+    def as_dict(self) -> Dict[str, List[float]]:
+        return {
+            'accuracy': self.accuracy,
+            'precision': self.precision,
+            'recall': self.recall,
+            'f1': self.f1
+        }
+
+
+def get_stats(true: np.ndarray, pred: np.ndarray) -> AttackResult:
+    """
+    Returns the statistics comparing the true and predicted values.
+    """
+    assert len(true.shape) == 1, 'Must provide a 1d array of true labels'
+    assert len(pred.shape) == 1, 'Must provide a 1d array of predicted labels'
+
+    return AttackResult(accuracy=accuracy_score(y_true=true, y_pred=pred),
+                        precision=precision_score(y_true=true, y_pred=pred, average='macro', zero_division=0),
+                        recall=recall_score(y_true=true, y_pred=pred, average='macro'),
+                        f1=f1_score(y_true=true, y_pred=pred, average='macro'))
 
 
 def create_dataset(message_sizes: List[int], labels: List[int], window_size: int, num_samples: int, rand: np.random.RandomState) -> Tuple[np.ndarray, np.ndarray]:
@@ -45,13 +79,13 @@ def create_dataset(message_sizes: List[int], labels: List[int], window_size: int
     for label in bytes_dist.keys():
         sizes = bytes_dist[label]
 
-        # print('Label: {0}, Num Bytes: {1} (Std: {2}, Count: {3})'.format(label, np.average(sizes), np.std(sizes), len(sizes)))
-
         num_to_create = int(round(num_samples * (len(sizes) / num_messages)))
 
         for _ in range(num_to_create):
             raw_sizes = rand.choice(sizes, size=window_size)  # [D]
-            features = [np.average(raw_sizes), np.median(raw_sizes), np.std(raw_sizes), np.max(raw_sizes), np.min(raw_sizes), geometric_mean(raw_sizes)]
+
+            iqr = np.percentile(raw_sizes, 75) - np.percentile(raw_sizes, 25)
+            features = [np.average(raw_sizes), np.std(raw_sizes), np.median(raw_sizes), np.max(raw_sizes), np.min(raw_sizes), iqr, geometric_mean(raw_sizes)]
 
             inputs.append(np.expand_dims(features, axis=0))
             output.append(label)
@@ -59,99 +93,63 @@ def create_dataset(message_sizes: List[int], labels: List[int], window_size: int
     return np.vstack(inputs), np.vstack(output).reshape(-1)
 
 
-def fit_attack_model(message_sizes: List[int], labels: List[int], window_size: int, num_samples: int, train_frac: float, val_frac: float, name: str, save_folder: str):
+def fit_attack_model(message_sizes: np.array, labels: np.array, window_size: int, num_samples: int, name: str, save_folder: str):
     """
     Fits the attacker model which predicts labels from message sizes.
-
-    Args:
-        inputs: A [
     """
     assert len(message_sizes) == len(labels), 'Must provide the same number of messages ({0}) as labels ({1}).'.format(len(message_sizes), len(labels))
 
-    # Shuffle the data together
     rand = np.random.RandomState(582)
-    
-    zipped = list(zip(message_sizes, labels))
-    rand.shuffle(zipped)
-    message_sizes, labels = zip(*zipped)
 
-    # Split the data
-    train_split = int(train_frac * len(message_sizes))
-    val_split = int((train_frac + val_frac) * len(message_sizes))
+    # Create the data-set
+    inputs, outputs = create_dataset(message_sizes=message_sizes,
+                                     labels=labels,
+                                     window_size=window_size,
+                                     num_samples=num_samples,
+                                     rand=rand)
 
-    train_sizes = message_sizes[:train_split]
-    val_sizes = message_sizes[train_split:val_split]
-    test_sizes = message_sizes[val_split:]
-    
-    train_labels = labels[:train_split]
-    val_labels = labels[train_split:val_split]
-    test_labels = labels[val_split:]
+    train_results = AttackResultList()
+    test_results = AttackResultList()
+    most_freq_results = AttackResultList()
 
-    # Create the data-sets
-    num_train = int(train_frac * num_samples)
-    num_val = int(val_frac * num_samples)
-    num_test = num_samples - num_train - num_val
+    kf = KFold(n_splits=5, random_state=rand, shuffle=True)
 
-    train_inputs, train_outputs = create_dataset(message_sizes=train_sizes,
-                                                 labels=train_labels,
-                                                 window_size=window_size,
-                                                 num_samples=num_train,
-                                                 rand=rand)
+    for train_idx, test_idx in kf.split(inputs):
+        # Split the data
+        train_inputs, test_inputs = inputs[train_idx], inputs[test_idx]
+        train_labels, test_labels = outputs[train_idx], outputs[test_idx]
 
-    val_inputs, val_outputs = create_dataset(message_sizes=val_sizes,
-                                             labels=val_labels,
-                                             window_size=window_size,
-                                             num_samples=num_val,
-                                             rand=rand)
+        # Create the fit the model
+        clf = AdaBoostClassifier(n_estimators=50)
+        clf.fit(train_inputs, train_labels)
 
-    test_inputs, test_outputs = create_dataset(message_sizes=test_sizes,
-                                               labels=test_labels,
-                                               window_size=window_size,
-                                               num_samples=num_test,
-                                               rand=rand)
+        # Get the training and testing predictions
+        train_pred = clf.predict(train_inputs)
+        test_pred = clf.predict(test_inputs)
 
-    # Scale the inputs
-    scaler = StandardScaler()
-    train_inputs = scaler.fit_transform(train_inputs)
-    val_inputs = scaler.transform(val_inputs)
-    test_inputs = scaler.transform(test_inputs)
+        train_stats = get_stats(true=train_labels, pred=train_pred)
+        test_stats = get_stats(true=test_labels, pred=test_pred)
 
-    clf = AttackClassifier(name=name)
-    
-    most_freq_label = np.bincount(train_outputs, minlength=np.amax(train_outputs)).argmax()
-    most_freq_labels = [most_freq_label for _ in test_outputs]
-    most_freq_acc = accuracy_score(y_true=test_outputs, y_pred=most_freq_labels)
+        train_results.append(train_stats)
+        test_results.append(test_stats)
 
-    print('Most Freq Accuracy: {0:.5f}'.format(most_freq_acc))
+        # Get the most frequent accuracy based on the training set
+        most_freq_label = np.bincount(train_labels, minlength=np.amax(train_labels)).argmax()
+        most_freq_labels = np.array([most_freq_label for _ in test_labels])
+        most_freq_stats = get_stats(true=test_labels, pred=most_freq_labels)
 
-    clf.fit(train_inputs=train_inputs,
-            train_labels=train_outputs,
-            val_inputs=val_inputs,
-            val_labels=val_outputs,
-            num_epochs=NUM_EPOCHS,
-            save_folder=save_folder)
+        train_results.append(train_stats)
+        test_results.append(test_stats)
+        most_freq_results.append(most_freq_stats)
 
-    # Load the best model
-    clf.restore(save_folder=save_folder)
+    print('Train Accuracy: {0:.5f}'.format(np.average(train_results.accuracy)))
+    print('Test Accuracy: {0:.5f}'.format(np.average(test_results.accuracy)))
+    print('Most Freq Accuracy: {0:.5f}'.format(np.average(most_freq_results.accuracy)))
 
-    train_accuracy = clf.accuracy(train_inputs, train_outputs)
-    val_accuracy = clf.accuracy(val_inputs, val_outputs)
-    test_accuracy = clf.accuracy(test_inputs, test_outputs)
-    test_macro_f1 = clf.macro_f1(test_inputs, test_outputs)
-
-    print('Train Accuracy: {0:.5f} ({1})'.format(train_accuracy, len(train_inputs)))
-    print('Val Accuracy: {0:.5f} ({1})'.format(val_accuracy, len(val_inputs)))
-    print('Attack Accuracy: {0:.5f} ({1})'.format(test_accuracy, len(test_inputs)))
-    print('Most Freq Accuracy: {0:.5f}'.format(most_freq_acc))
-
-    return dict(train_accuracy=train_accuracy,
-                val_accuracy=val_accuracy,
-                test_accuracy=test_accuracy,
-                test_macro_f1=test_macro_f1,
-                num_train=len(train_inputs),
-                num_val=len(val_inputs),
-                num_test=len(test_inputs),
-                most_freq_accuracy=most_freq_acc)
+    return dict(train=train_results.as_dict(),
+                test=test_results.as_dict(),
+                most_freq=most_freq_results.as_dict(),
+                count=len(inputs))
 
 
 if __name__ == '__main__':
@@ -179,11 +177,9 @@ if __name__ == '__main__':
         model_name = os.path.basename(path)
         model_name = model_name.split('.')[0]
 
-        attack_result = fit_attack_model(message_sizes=policy_result['num_bytes'],
-                                         labels=policy_result['labels'],
+        attack_result = fit_attack_model(message_sizes=np.array(policy_result['num_bytes']),
+                                         labels=np.array(policy_result['labels']),
                                          window_size=args.window_size,
-                                         train_frac=0.7,
-                                         val_frac=0.15,
                                          num_samples=args.num_samples,
                                          name=model_name,
                                          save_folder=save_folder)
