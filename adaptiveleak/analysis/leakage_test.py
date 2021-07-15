Fix mutual information calculation

adaptiveleak/analysis/leakage_test.py
@@ -1,7 +1,9 @@
 import numpy as np
 import math
 import scipy.stats as stats
+import time
 from argparse import ArgumentParser
+from sklearn.metrics import mutual_info_score
 from collections import Counter, defaultdict, OrderedDict, namedtuple
 from typing import Dict, List, DefaultDict, Optional, Tuple, Iterable
 
@@ -10,122 +12,43 @@ from adaptiveleak.utils.constants import POLICIES, SMALL_NUMBER
 from adaptiveleak.utils.file_utils import read_json_gz, iterate_dir, save_json_gz
 
 
-JointKey = namedtuple('JointKey', ['label', 'size'])
+BIN_FACTOR = 10
 
 
-def get_label_distribution(byte_dist: Dict[int, List[int]]) -> Dict[int, float]:
-    """
-    Finds the empirical frequency of each label.
+def compute_mutual_information(labels: List[int], byte_counts: List[int]) -> float:
+    # Compute the joint distribution via a histogram
+    num_samples = len(labels)
+    byte_bins = int(num_samples / BIN_FACTOR)
+    label_bins = np.amax(labels) + 1
 
-    Args:
-        byte_dist: A dictionary mapping label -> list of message sizes (in bytes)
-    Returns:
-        A dictionary mapping the label to its frequency in the data.
-    """
-    counts: Dict[int, int] = dict()
-    total = 0
+    joint_hist, _, _ = np.histogram2d(labels, byte_counts, bins=(label_bins, byte_bins))
 
-    for label, sizes in byte_dist.items():
-        counts[label] = len(sizes)
-        total += len(sizes)
+    # Compute the empirical mutual information
+    mutual_information = mutual_info_score(None, None, contingency=joint_hist)
 
-    return {label: count / total for label, count in counts.items()}
+    return mutual_information
 
 
-def get_size_distribution(byte_dist: Dict[int, List[int]]) -> Dict[int, float]:
-    """
-    Finds the empirical frequency of each message size.
-
-    Args:
-        byte_dict: A dictionary mapping label -> list of message sizes (in bytes)
-    Returns:
-        A dictionary mapping the message size to its frequency in the data.
-    """
-    counts: Counter = Counter()
-    total = 0
-
-    for sizes in byte_dist.values():
-        for size in sizes:
-            counts[size] += 1
-
-        total += len(sizes)
-
-    return {size: count / total for size, count in counts.items()}
-
-
-def get_joint_distribution(byte_dist: Dict[int, List[int]]) -> Dict[JointKey, float]:
-    """
-    Forms the empirical joint distribution between message size and label.
-
-    Args:
-        byte_dist: A dictionary mapping label -> list of message sizes (in bytes)
-    Returns:
-        A dictionary with key (label, size) and value equal to the frequency in the data.
-    """
-    counts: Counter = Counter()
-    total = 0
-
-    for label, sizes in byte_dist.items():
-        for size in sizes:
-            key = JointKey(label=label, size=size)
-            counts[key] += 1
-            total += 1
-
-    return {key: count / total for key, count in counts.items()}
-
-
-def mutual_information(byte_dist: Dict[int, List[int]]) -> float:
-    # Compute the required empirical distributions
-    label_dist = get_label_distribution(byte_dist=byte_dist)
-    size_dist = get_size_distribution(byte_dist=byte_dist)
-    joint_dist = get_joint_distribution(byte_dist=byte_dist)
-
-    # Calculate the mutual information
-    information = 0.0
-
-    for label, size in joint_dist.keys():
-        # Unpack the required probabilities
-        label_prob = label_dist[label]
-        size_prob = size_dist[size]
-
-        key = JointKey(label=label, size=size)
-        joint_prob = joint_dist[key]
-
-        information += joint_prob * np.log((joint_prob) / (label_prob * size_prob + SMALL_NUMBER))
-
-    # Return the mutual information
-    return information
-
-
-def randomize_distribution(byte_dist: Dict[int, List[int]], rand: np.random.RandomState, num_trials: int) -> Iterable[DefaultDict[int, List[int]]]:
+def randomize_distribution(sizes: List[int], rand: np.random.RandomState, num_trials: int) -> Iterable[List[int]]:
     """
     Creates random permutations of the given message size distribution.
     """
-    # Flatten the data
-    labels: List[int] = []
-    sizes: List[int] = []
-
-    for label, label_sizes in byte_dist.items():
-        for size in label_sizes:
-            labels.append(label)
-            sizes.append(size)
-
     for _ in range(num_trials):
         # Shuffle the sizes
         rand.shuffle(sizes)
 
         # Create the new distribution
-        perm_dist: DefaultDict[int, List[int]] = defaultdict(list)
-        
-        for label, size in zip(labels, sizes):
-            perm_dist[label].append(size)
-
-        yield perm_dist
+        yield sizes
 
 
 def compute_p_value(information: float, randomized_information: List[float]) -> float:
     num_greater_eq = sum(int(r >= information) for r in randomized_information)
     return (num_greater_eq + 1) / (len(randomized_information) + 1)
+
+
+def compute_entropy(counts: List[int]) -> float:
+    probs = counts / np.sum(counts)
+    return -1 * sum((p * np.log(p)) for p in probs if p > SMALL_NUMBER)
 
 
 def run_test(byte_dist: Dict[int, List[int]], num_trials: int) -> Tuple[float, float]:
@@ -135,25 +58,46 @@ def run_test(byte_dist: Dict[int, List[int]], num_trials: int) -> Tuple[float, f
     Args:
         byte_dist: A map of label -> List of message sizes (in bytes)
     """
-    # Get the mutual information of the existing distribution
-    information = mutual_information(byte_dist)
+     # Extract a list of labels and byte counts
+    labels: List[int] = []
+    byte_counts: List[int] = []
+
+    for label, counts in byte_dist.items():
+        for count in counts:
+            labels.append(label)
+            byte_counts.append(count)
+
+    # Compute the entropy of each distribution
+    label_counts = np.bincount(labels).astype(float)
+    label_entropy = compute_entropy(counts)
+
+    num_bins = int(len(labels) / BIN_FACTOR)
+    byte_hist, _ = np.histogram(byte_counts, bins=num_bins)
+    byte_entropy = compute_entropy(byte_hist)
+
+    # Get the (normalized) mutual information of the existing distribution
+    information = compute_mutual_information(labels, byte_counts)
+    norm_information = (2 * information) / (label_entropy + byte_entropy)
 
     # Get the mutual information of the randomized distributions
     randomized_information: List[float] = []
     rand = np.random.RandomState(seed=23634)
 
-    for perm_dist in randomize_distribution(byte_dist, rand=rand, num_trials=num_trials):
-        randomized_information.append(mutual_information(perm_dist))
+    for perm_sizes in randomize_distribution(byte_counts, rand=rand, num_trials=num_trials):
+        perm_information = compute_mutual_information(labels, perm_sizes)
+        norm_perm_information = (2 * perm_information) / (label_entropy + byte_entropy)
+
+        randomized_information.append(norm_perm_information)
 
     # Get the percentile of the observed mutual information within the randomized distribution
-    perc = stats.percentileofscore(randomized_information, information)
-    p_value = compute_p_value(information=information, randomized_information=randomized_information)
+    perc = stats.percentileofscore(randomized_information, norm_information)
+    p_value = compute_p_value(information=norm_information, randomized_information=randomized_information)
 
     return {
         'percentile': perc,
         'p_value': p_value,
         'randomized_mi': { 'avg': float(np.average(randomized_information)), 'std': float(np.std(randomized_information)) },
-        'mutual_information': float(information),
+        'norm_mutual_information': float(norm_information),
         'num_trials': num_trials
     }
 
@@ -161,14 +105,11 @@ def run_test(byte_dist: Dict[int, List[int]], num_trials: int) -> Tuple[float, f
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--dates', type=str, required=True, nargs='+')
-    parser.add_argument('--precision', type=float, required=True)
+    parser.add_argument('--trials', type=int, required=True)
     parser.add_argument('--dataset', type=str, required=True)
     args = parser.parse_args()
 
-    assert args.precision > 0, 'Precision must be positive'
-
-    # Compute the number of trials based on a conservative upper bound
-    num_trials = int(math.ceil(1.0 / (4 * args.precision**2)))
+    assert args.trials > 0, '# Trials must be positive'
 
     information_results: DefaultDict[str, Dict[float, float]] = defaultdict(dict)
 
@@ -183,13 +124,18 @@ if __name__ == '__main__':
             for label, byte_count in zip(labels, num_bytes):
                 byte_dist[label].append(byte_count)
 
-            name = '{0}_{1}'.format(model['policy']['policy_name'].lower(), model['policy']['encoding_mode'].lower())
+            encoding_mode = model['policy']['encoding_mode'].lower()
+
+            if encoding_mode in ('single_group', 'group_unshifted'):
+                continue
+
+            name = '{0}_{1}'.format(model['policy']['policy_name'].lower(), encoding_mode)
             energy_per_seq = model['policy']['energy_per_seq']
 
             if name not in ('adaptive_heuristic_standard', 'adaptive_deviation_standard', 'skip_rnn_standard'):
                 test_result = run_test(byte_dist, num_trials=1)
             else:
-                test_result = run_test(byte_dist, num_trials=num_trials)
+                test_result = run_test(byte_dist, num_trials=args.trials)
 
-            model['mututal_information'] = test_result
+            model['mutual_information'] = test_result
             save_json_gz(model, sim_file)